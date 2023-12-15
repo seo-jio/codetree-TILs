81bb754ff4d7e884e19a51f844e4ff4fa7903641
seo-jio <81673820+seo-jio@users.noreply.github.com>
2023-12-15 15:05:00 +0900

    Title: 부분수열 여부 판단하기, Time: 611ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 231215/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] A, B;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(isPossible());
+    }
+
+    private static String isPossible() {
+        int a=0;
+        for(int b=0; b<M; b++) {
+            while(a<N) {
+                if(A[a] == B[b]) {
+                    a++;
+                    break;
+                }
+
+                a++;
+            }
+
+            if(B[b] != A[a -1]) {
+                return "No";
+            }
+        }
+
+
+        return "Yes";
+    }
+}
