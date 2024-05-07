66538b76a04948c8b726c17cfe6658461bde51b1
seo-jio <81673820+seo-jio@users.noreply.github.com>
2024-05-07 19:30:50 +0900

    Title: 노드의 정수 계산, Time: 651ms, Memory: 53MB, Status: Passed - CodeTree

---
+++ 240507/노드의 정수 계산/calculating-an-integer-for-a-node.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] parents, dp, weights;
+    static boolean[] visited;
+    static int N;
+    static List<Integer>[] edges;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        parents = new int[N+1];
+        dp = new int[N+1];
+        weights = new int[N+1];
+        visited = new boolean[N+1];
+
+        edges = new List[N+1];
+        for(int i=0; i<N+1; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<N-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int t = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            
+            int cur = 2 + i;
+            weights[cur] = (t == 1) ? a : -a;
+
+            parents[cur] = p;
+            edges[p].add(cur);
+        }
+
+        dfs(1);
+
+        System.out.println(dp[1]);
+    }
+
+    private static void dfs(int cur) {
+        for(int i=0; i<edges[cur].size(); i++) {
+            int next = edges[cur].get(i);
+
+            if(visited[next]) {
+                continue;
+            }
+
+            visited[next] = true;
+            dfs(next);
+        }
+
+        int sum = weights[cur];
+        for(int i=0; i<edges[cur].size(); i++) {
+            int next = edges[cur].get(i);
+
+            if(dp[next] < 0) { 
+                continue;
+            }
+
+            sum += dp[next];
+        }
+
+        dp[cur] = sum;
+    }
+}
